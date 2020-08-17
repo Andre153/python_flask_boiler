f55eb082053eee00c41763fa93b48839447538ba
Andre Coetzee <andrecoetzee153@gmail.com>
2020-08-17 07:19:08 -0400

    added database

--- setup.py
+++ setup.py
@@ -5,21 +5,34 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 
-class Config:
-
+class DevelopConfig:
     SECRET_KEY = environ.get('SECRET_KEY')
     FLASK_ENV = environ.get('FLASK_ENV')
     FLASK_APP = 'app.py'
 
-    # Flask-Assets
-    LESS_BIN = environ.get('LESS_BIN')
-    ASSETS_DEBUG = True
-    LESS_RUN_IN_DEBUG = True
+    # DB
+    SQLALCHEMY_DATABASE_URI = environ.get("POSTGRES_URI")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Static Assets
-    STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = 'templates'
-    COMPRESSOR_DEBUG = True
 
-    # API
-    BEST_BUY_API_KEY = environ.get('BEST_BUY_API_KEY')
+class StagingConfig:
+    SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_APP = 'app.py'
+
+    # DB
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig:
+    SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_APP = 'app.py'
+
+    # DB
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from flask_assets import Environment
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
+    print("creating app")
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('setup.Config')
+    app.config.from_object('setup.DevelopConfig')
     assets = Environment()
     assets.init_app(app)
+    db.init_app(app)
 
     with app.app_context():
         from .user import user
-
         app.register_blueprint(user.user_bp)
+        from .user.model import User
 
+        db.create_all()
         return app
